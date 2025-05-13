bd89db2722c9acace5135031990b0975256a5195
adem-mehdioui <mohamed.adem.mehdioui@gmail.com>
2025-05-13 12:09:45 +0200

    adding calculator routes that perform the operations

--- api_try.py
+++ api_try.py
@@ -1,13 +1,41 @@
+# filepath: d:\Ecole HETIC\MD5\data-apps\Dev\Flask_API\api_try.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return "Mon premier API"
+    return render_template('web.html')
 
 
 
+@app.route('/name', methods=['GET', 'POST'])
+def myname():
+   return "Adem MEHDIOUI."
+
+@app.route('/promotion_name', methods=['GET', 'POST'])
+def promotion_name():
+   return "Promotion MD5 204/2025"
+
+
+# calculator
+@app.route('/add/<int:a>/<int:b>', methods=['GET', 'POST'])
+def calculator(a, b):
+    return str(a + b)
+
+@app.route('/subtract/<int:a>/<int:b>', methods=['GET', 'POST'])
+def subtract(a, b):
+    return str(a - b)
+
+@app.route('/multiply/<int:a>/<int:b>', methods=['GET', 'POST'])
+def multiply(a, b):
+    return str(a * b)
+
+@app.route('/divide/<int:a>/<int:b>', methods=['GET', 'POST'])
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return str(a / b)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
